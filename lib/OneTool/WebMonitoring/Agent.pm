7ebc465e4edae7c0fbf784113f6c6ac1e8b33dbd
Sebastien Thebert <sebthebert@gmail.com>
2014-07-06 16:13:29 +0200

    Update Agent.pm

--- lib/OneTool/WebMonitoring/Agent.pm
+++ lib/OneTool/WebMonitoring/Agent.pm
@@ -1,3 +1,18 @@
 package OneTool::WebMonitoring::Agent;
 
+=head1 NAME
+
+OneTool::WebMonitoring::Agent - OneTool WebMonitoring Agent module
+
+=cut
+
+use strict;
+use warnings;
+
 1;
+
+=head1 AUTHOR
+
+Sebastien Thebert <contact@onetool.pm>
+
+=cut
